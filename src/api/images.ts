9f891f696ec56ee3c4d0eb9aac899df20cf72d98
faisal <nmr.njd22@gmail.com>
2022-09-30 17:17:52 +0300

    Importing sharp package and test it by resize

--- src/api/images.ts
+++ src/api/images.ts
@@ -1,13 +1,24 @@
 import express from 'express';
 import loggers from './loggers';
-
+import sharp from 'sharp';
+import { promises as fsPromises } from 'fs';
 const images = express.Router(); //Create a router
+
+const oldImage = 'images/fjord.jpg';
+const resizeImage = 'images/resized-fjord.jpg';
 
 /**
  * To send data
  */
-images.get('/images', loggers, (req, res) => {
+images.get('/images', loggers, async (req, res) => {
   res.send('Working from /images');
+
+  try {
+    //resize the image
+    const resize = await sharp(oldImage).resize(300, 300).toFile(resizeImage);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 export default images; //export the router
