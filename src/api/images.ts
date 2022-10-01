4256a873badf22b2e0db4162664387ff17d4b999
Faisal <68514108+faisalkh90@users.noreply.github.com>
2022-10-01 15:42:44 +0300

    Added check condition, save the query and display the resized image to the user

--- src/api/images.ts
+++ src/api/images.ts
@@ -2,22 +2,38 @@ import express from 'express';
 import loggers from './loggers';
 import sharp from 'sharp';
 import { promises as fsPromises } from 'fs';
+import path from 'path';
 const images = express.Router(); //Create a router
 
-const oldImage = 'images/fjord.jpg';
-const resizeImage = 'images/resized-fjord.jpg';
+const resizeImage = 'image/resized-fjord.jpg';
 
 /**
  * To send data
  */
-images.get('/images', loggers, async (req, res) => {
-  res.send('Working from /images');
 
-  try {
-    //resize the image
-    const resize = await sharp(oldImage).resize(300, 300).toFile(resizeImage);
-  } catch (err) {
-    console.log(err);
+images.get('/images', loggers, async (req, res) => {
+  //Condition that the endpoint should be open with 200 'OK'
+  if (res.statusCode === 200) {
+    try {
+      //Create an object and assign the queries
+      let queryObj = req.query;
+      let fileName = queryObj.filename;
+      let height = Number(queryObj.height);
+      let width = Number(queryObj.width);
+
+      //resize the image and wait until save resize image to new file
+      const resize = await sharp(`image/${fileName}.jpg`)
+        .resize(width, height)
+        .toFile(resizeImage);
+      return res.sendFile(path.resolve(`${resizeImage}`)); //display the resized image to the browser
+    } catch (err) {
+      //if the user doesn't provide query, the catch will handle it
+      return res.send(
+        'Please provide a query after "/". For example : ?filename=fjord&width=250&height=250'
+      );
+    }
+  } else {
+    return res.send(res.statusMessage);
   }
 });
 
