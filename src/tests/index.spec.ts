0130a6a12859875366ffd9da24d0fd484d267406
faisal <nmr.njd22@gmail.com>
2022-10-02 09:25:32 +0300

    Added all test scenario

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,9 +1,24 @@
 import app from '../index';
-import supertest from 'supertest';
+import request from 'supertest'; //testing HTTP
+import { image_processing } from '../api/images';
+import express from 'express';
 
-describe('Testing the home page endpoint', function () {
-  it('returns 200', async function () {
-    // status code should be 200 `OK`
-    await supertest(app).get('/api').expect(200);
+describe('Testing the api/images endpoint', () => {
+  it('returns 200 OK', async () => {
+    await request(app).get('/api/images').expect(200); //Check the statusCode and should be 200"OK"
+  });
+
+  it('Expected 404 not found.', async () => {
+    await request(app).get('/api/images/a').expect(404);
+  });
+});
+
+describe('Testing image processing function', () => {
+  const req = express.request;
+  const res = express.response;
+  it('Expected function to not throw an exception.', async () => {
+    expect(() => {
+      image_processing(req, res);
+    }).not.toThrow();
   });
 });
