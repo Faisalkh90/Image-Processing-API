80a6e833ee3b20955e97a244fb009df26278a295
faisal <nmr.njd22@gmail.com>
2022-10-02 16:08:18 +0300

    Check for query parameters like filename, height and width before sending image for processing

--- src/api/images.ts
+++ src/api/images.ts
@@ -14,33 +14,43 @@ async function image_processing(req: express.Request, res: express.Response) {
   //Create an object and assign the queries
 
   let queryObj = req.query;
-  let fileName: string = String(queryObj.filename);
+  const fileName: string = String(queryObj.filename);
   let height: number = Number(queryObj.height);
   let width: number = Number(queryObj.width);
-
+  if (fileName !== 'fjord') {
+    return res.send('Input file is missing:fjord1. It should to be "fjord"');
+  }
   //resize the image and wait until save resize image to new file
   const resize = await sharp(`image/${fileName}.jpg`)
     .resize(width, height)
     .toFile(resizeImage);
   return res.sendFile(path.resolve(`${resizeImage}`)); //display the resized image to the browser
-
-  // console.error(err);
 }
-images.get('/images', loggers, async (req, res) => {
-  //Condition that the endpoint should be open with 200 'OK'
-  if (res.statusCode === 200) {
-    try {
-      await image_processing(req, res);
-    } catch (err) {
-      //if the user doesn't provide query, the catch will handle it
-      return res.send(
-        'Please provide a query after "/". For example : ?filename=fjord&width=250&height=250'
-      );
+images.get(
+  '/images',
+  loggers,
+  async (req: express.Request, res: express.Response) => {
+    //Condition that the endpoint should be open with 200 'OK'
+    if (res.statusCode === 200) {
+      try {
+        await image_processing(req, res);
+      } catch (err) {
+        // console.log(err);
+        if (err instanceof Error) {
+          //Check for query parameters like filename, height and width before sending image for processing.
+          return res.send(err.message);
+        } else {
+          //if the user doesn't provide query, the catch will handle it
+          return res.send(
+            'Please provide a query after "/". For example : ?filename=fjord&width=250&height=250'
+          );
+        }
+      }
+    } else {
+      return res.send(res.statusMessage);
     }
-  } else {
-    return res.send(res.statusMessage);
   }
-});
+);
 
 export default images; //export the router
 export { image_processing };
