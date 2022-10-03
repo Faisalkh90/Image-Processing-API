3c7231e0f9cfec6f8efe779b46b90c402a66006d
faisal <nmr.njd22@gmail.com>
2022-10-03 15:52:46 +0300

    Added welcoming

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ const port = 3000;
 app.listen(port, (): void => {
   console.log(`Lionising on http://localhost:${port}`);
 });
-
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Welcome to Image Processing API!');
+});
 app.use('/api', routes); //use the route's module as middleware.
 export default app;
