f695cd330b02e811919112cbb634794b8422bb43
faisal <nmr.njd22@gmail.com>
2022-10-04 17:41:38 +0300

    Added more test cases

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,6 +1,6 @@
 import app from '../index';
 import request from 'supertest'; //testing HTTP
-import image_processing from '../processing-image';
+import { resize, image_processing, checkExist } from '../processing-image';
 import express from 'express';
 
 describe('Testing the api/images endpoint', () => {
@@ -13,12 +13,18 @@ describe('Testing the api/images endpoint', () => {
   });
 });
 
-describe('Testing image processing function', () => {
-  const req = express.request;
-  const res = express.response;
-  it('Expected function to not throw an exception.', async () => {
-    expect(() => {
-      image_processing(req, res);
+describe('Testing image processing functions', () => {
+  // const req = express.Request;
+  // const res = express.response;
+  // let queryObj = await req.query;
+
+  it('Expected resize function to not throw an exception.', async () => {
+    expect(async () => {
+      await resize('fjord', 200, 200);
     }).not.toThrow();
+  });
+  it('Check image name if exist', async () => {
+    let fileName = await checkExist('fjord');
+    expect(fileName).toBeTruthy();
   });
 });
